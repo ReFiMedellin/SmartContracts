1b74dd6801525cf2f487d6205a7322cda3699e20
Luis_ <73004377+Another-DevX@users.noreply.github.com>
2023-12-07 20:01:44 -0500

    Update celoLend.sol

--- celoLend.sol
+++ celoLend.sol
@@ -1,4 +1,3 @@
-
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
@@ -9,19 +8,13 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.4.0/contr
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract SmartContractCELO is Pausable, Ownable, ReentrancyGuard {
-    uint256 internal constant INTEREST_RATE_PER_DAY = 16308;
-    uint256 internal constant INTEREST_PERIOD = 24 hours;
-
-    IERC20 internal cUSDToken;
-    uint256 public totalFunds;
-    uint256 public totalInterest;
-
-    constructor(address _cUSDTokenAddress) {
-        cUSDToken = IERC20(_cUSDTokenAddress);
-    }
+    /*****************************************
+     *             Custom Types              *
+     *****************************************/
 
     struct Lending {
         uint256 amount;
+        uint256 initialAmount;
         uint256 startDate;
         uint256 blockMonths;
     }
@@ -31,9 +24,24 @@ contract SmartContractCELO is Pausable, Ownable, ReentrancyGuard {
         Lending[] lendings;
     }
 
+    /*****************************************
+     *               Storage                 *
+     *****************************************/
+
+    uint256 internal constant INTEREST_RATE_PER_DAY = 16308;
+    uint256 internal constant INTEREST_PERIOD = 24 hours;
+
+    IERC20 internal cUSDToken;
+    uint256 public totalFunds;
+    uint256 public totalInterest;
+
     mapping(address => bool) public whitelist;
     mapping(address => uint256) public property;
     mapping(address => Lender) public lenders;
+
+    /*****************************************
+     *                Events                 *
+     *****************************************/
 
     event WhitelistedUserAdded(address indexed user);
     event WhitelistedUserRemoved(address indexed user);
@@ -51,6 +59,35 @@ contract SmartContractCELO is Pausable, Ownable, ReentrancyGuard {
     );
     event LoanFullyRepaid(address indexed borrower, uint256 lendingIndex);
 
+    constructor(address _cUSDTokenAddress) {
+        cUSDToken = IERC20(_cUSDTokenAddress);
+    }
+
+    /*****************************************
+     *                Public                 *
+     *****************************************/
+
+    function simulateInterestAccrual(uint256 _amount, uint256 _months)
+        public
+        pure
+        returns (uint256)
+    {
+        uint256 compoundedAmount = _amount;
+        uint256 ratePerPeriod = INTEREST_RATE_PER_DAY;
+        uint256 periods = _months * 30;
+
+        for (uint256 i = 0; i < periods; i++) {
+            compoundedAmount =
+                (compoundedAmount * (10**8 + ratePerPeriod)) /
+                10**8;
+        }
+        return compoundedAmount;
+    }
+
+    /*****************************************
+     *               External                *
+     *****************************************/
+
     function capitalize(uint256 _amount) external whenNotPaused {
         require(_amount > 0, "Amount must be greater than 0");
         require(
@@ -59,30 +96,6 @@ contract SmartContractCELO is Pausable, Ownable, ReentrancyGuard {
         );
         totalFunds += _amount;
         property[msg.sender] += _amount;
-    }
-
-    function accrueInterest(address _lender, uint256 _lendingIndex) internal {
-        Lending storage lending = lenders[_lender].lendings[_lendingIndex];
-        uint256 timeElapsed = block.timestamp - lending.startDate;
-
-        if (timeElapsed >= INTEREST_PERIOD) {
-            uint256 periodsElapsed = timeElapsed / INTEREST_PERIOD;
-            uint256 ratePerPeriod = INTEREST_RATE_PER_DAY; // Esta es la tasa diaria sin escalar
-
-            uint256 compoundInterest = lending.amount;
-            for (uint256 i = 0; i < periodsElapsed; i++) {
-                // Escalar y luego desescalar para prevenir el redondeo a cero.
-                compoundInterest =
-                    (compoundInterest * (10**8 + ratePerPeriod)) /
-                    10**8;
-            }
-
-            uint256 interestAmount = compoundInterest - lending.amount;
-            lending.amount = compoundInterest;
-            totalInterest += interestAmount;
-
-            lending.startDate += periodsElapsed * INTEREST_PERIOD;
-        }
     }
 
     function payDebt(uint256 _lendingIndex, uint256 _amount)
@@ -100,12 +113,11 @@ contract SmartContractCELO is Pausable, Ownable, ReentrancyGuard {
         require(lending.amount > 0, "No active debt to pay");
         require(_amount <= lending.amount, "Payment exceeds the debt amount");
         lending.amount -= _amount;
-
         require(
             cUSDToken.transferFrom(msg.sender, address(this), _amount),
             "Transfer failed"
         );
-
+        lender.aggreedQuota += _amount;
         emit PaymentMade(msg.sender, _lendingIndex, _amount, lending.amount);
 
         if (lending.amount == 0) {
@@ -129,19 +141,8 @@ contract SmartContractCELO is Pausable, Ownable, ReentrancyGuard {
         return currentFunds;
     }
 
-    function getCurrentQuota(address _lender) external view returns (uint256){
+    function getCurrentQuota(address _lender) external view returns (uint256) {
         return lenders[_lender].aggreedQuota;
-    }
-
-    function removeLending(address _lender, uint256 _lendingIndex) internal {
-        Lender storage lender = lenders[_lender];
-        uint256 lastLendingIndex = lender.lendings.length - 1;
-
-        if (_lendingIndex < lastLendingIndex) {
-            lender.lendings[_lendingIndex] = lender.lendings[lastLendingIndex];
-        }
-
-        lender.lendings.pop();
     }
 
     function increaseQuota(address _lender, uint256 _amount)
@@ -163,8 +164,17 @@ contract SmartContractCELO is Pausable, Ownable, ReentrancyGuard {
     {
         require(_amount > 0, "Decrease amount must be greater than 0");
         require(_lender != address(0), "Invalid lender address");
-
+        uint256 currentDue = 0;
         Lender storage lender = lenders[_lender];
+
+        for (uint256 i = 0; i < lender.lendings.length; i++) {
+            currentDue += lender.lendings[i].amount;
+        }
+
+        require(
+            currentDue <= lender.aggreedQuota - _amount,
+            "Decrease amount exceeds current due"
+        );
         require(
             lender.aggreedQuota >= _amount,
             "Decrease amount exceeds the agreed quota"
@@ -224,6 +234,7 @@ contract SmartContractCELO is Pausable, Ownable, ReentrancyGuard {
         lenders[msg.sender].lendings.push(
             Lending({
                 amount: _amount,
+                initialAmount: _amount,
                 startDate: block.timestamp,
                 blockMonths: _blockMonths
             })
@@ -232,10 +243,16 @@ contract SmartContractCELO is Pausable, Ownable, ReentrancyGuard {
         require(cUSDToken.transfer(msg.sender, _amount), "Transfer failed");
         emit LoanRequested(msg.sender, _amount, _blockMonths);
     }
-    function addToWhitelist(address _user) external onlyOwner {
+
+    function addToWhitelist(address _user, uint256 _amount) external onlyOwner {
         require(_user != address(0), "Invalid address");
         require(!whitelist[_user], "User already whitelisted");
         whitelist[_user] = true;
+        Lender storage lender = lenders[_user];
+        lender.aggreedQuota += _amount;
+
+        emit QuotaAdjusted(_user, lender.aggreedQuota);
+
         emit WhitelistedUserAdded(_user);
     }
 
@@ -284,20 +301,42 @@ contract SmartContractCELO is Pausable, Ownable, ReentrancyGuard {
         );
     }
 
-    function simulateInterestAccrual(uint256 _amount, uint256 _months)
-        public
-        pure
-        returns (uint256)
-    {
-        uint256 compoundedAmount = _amount;
-        uint256 ratePerPeriod = INTEREST_RATE_PER_DAY;
-        uint256 periods = _months * 30;
+    /*****************************************
+     *               Internal                *
+     *****************************************/
 
-        for (uint256 i = 0; i < periods; i++) {
-            compoundedAmount =
-                (compoundedAmount * (10**8 + ratePerPeriod)) /
-                10**8;
+    function accrueInterest(address _lender, uint256 _lendingIndex) internal {
+        Lending storage lending = lenders[_lender].lendings[_lendingIndex];
+        uint256 timeElapsed = block.timestamp - lending.startDate;
+
+        if (timeElapsed >= INTEREST_PERIOD) {
+            uint256 periodsElapsed = timeElapsed / INTEREST_PERIOD;
+            uint256 ratePerPeriod = INTEREST_RATE_PER_DAY; // Esta es la tasa diaria sin escalar
+
+            uint256 compoundInterest = lending.amount;
+            for (uint256 i = 0; i < periodsElapsed; i++) {
+                // Escalar y luego desescalar para prevenir el redondeo a cero.
+                compoundInterest =
+                    (compoundInterest * (10**8 + ratePerPeriod)) /
+                    10**8;
+            }
+
+            uint256 interestAmount = compoundInterest - lending.amount;
+            lending.amount = compoundInterest;
+            totalInterest += interestAmount;
+
+            lending.startDate += periodsElapsed * INTEREST_PERIOD;
         }
-        return compoundedAmount;
+    }
+
+    function removeLending(address _lender, uint256 _lendingIndex) internal {
+        Lender storage lender = lenders[_lender];
+        uint256 lastLendingIndex = lender.lendings.length - 1;
+
+        if (_lendingIndex < lastLendingIndex) {
+            lender.lendings[_lendingIndex] = lender.lendings[lastLendingIndex];
+        }
+
+        lender.lendings.pop();
     }
 }
